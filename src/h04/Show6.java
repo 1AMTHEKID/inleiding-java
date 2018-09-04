ae94a1a01faa2258ee0a2370950695ead5c0e4f4
Pascual FG <pascualfrankel@hotmail.com>
2018-09-04 13:40:03 +0200

    Hele map geupdate, ivm teveel repo's en het overzicht houden van bestanden.

---
+++ src/h04/Show6.java
@@ -0,0 +1,21 @@
+package h04;
+import java.awt.*;
+import java.applet.*;
+
+public class Show6 extends Applet {
+    public void init() {setBackground(Color.black);}
+
+    public void paint(Graphics g){
+        g.setColor(Color.red);
+        g.drawRect(30,30,320,70);
+        g.fillRect(30,30,320,70);
+        g.setColor(Color.white);
+        g.drawRect(30,100,320,70);
+        g.fillRect(30,100,320,70);
+        g.setColor(Color.blue);
+        g.drawRect(30,170,320,70);
+        g.fillRect(30,170,320,70);
+
+    }
+
+}
